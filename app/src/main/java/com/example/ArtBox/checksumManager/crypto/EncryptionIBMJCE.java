30884bac0c3f8aa515b842b5e3467ccddc65986b
DesignDragon <prafuladate25@gmail.com>
2020-04-30 11:30:49 +0530

    updated

---
+++ app/src/main/java/com/example/ArtBox/checksumManager/crypto/EncryptionIBMJCE.java
@@ -0,0 +1,8 @@
+package com.example.ArtBox.checksumManager.crypto;
+
+
+public interface EncryptionIBMJCE {
+    String encryptIBMJCE(String var1, String var2) throws Exception;
+
+    String decryptIBMJCE(String var1, String var2) throws Exception;
+}
